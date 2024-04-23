02a47319cc4695570c8a61be963702340bdda7f0
IndraVanWynendaele <indra.vanwynendaele@student.hogent.be>
2024-04-23 21:11:05 +0200

    init

---
+++ src/main/java/com/springBoot/EWDJexamenopdracht/EwdJexamenopdrachtApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.EWDJexamenopdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwdJexamenopdrachtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwdJexamenopdrachtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBoot/EWDJexamenopdracht/EwdJexamenopdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.EWDJexamenopdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwdJexamenopdrachtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
