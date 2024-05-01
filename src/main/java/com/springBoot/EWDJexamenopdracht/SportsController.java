6cb826fcdf7995a9952babef8697b2eb918867cc
IndraVanWynendaele <indra.vanwynendaele@student.hogent.be>
2024-05-02 01:31:35 +0200

    url aanpassing gaat nu naar sports/id/games

--- src/main/java/com/springBoot/EWDJexamenopdracht/SportsController.java
+++ src/main/java/com/springBoot/EWDJexamenopdracht/SportsController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import repository.SportRepository;
@@ -19,6 +20,11 @@ public class SportsController {
 	public String showSportsPage(Model model) {
 		model.addAttribute("sportsList", sr.findAll());
 		return "sportsTable";
+	}
+	
+	@GetMapping(value = "/{id}/games")
+	public String showGamesPage(@PathVariable long id, Model model) {
+		return "gamesTable";
 	}
 	
 }
